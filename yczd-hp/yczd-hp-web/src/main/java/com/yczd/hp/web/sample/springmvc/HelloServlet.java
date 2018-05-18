f8e3d03219f14c224b26a1ce844c1457780ac53d
closing <zhu.yuxing@hotmail.com>
2018-05-18 17:41:01 +0900

    Create HelloServlet.java

---
+++ yczd-hp/yczd-hp-web/src/main/java/com/yczd/hp/web/sample/springmvc/HelloServlet.java
@@ -0,0 +1,39 @@
+package com.yczd.hp.web.sample.springmvc;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * SpringMVC Example
+ * @author ZHU
+ *
+ */
+@Controller
+@Scope("prototype")
+@RequestMapping("/springmvc")
+public class HelloServlet {
+
+	@RequestMapping(value = "/index", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView index() {
+		ModelAndView mov = new ModelAndView();
+		mov.setViewName("/index");
+		return mov;
+
+	}
+
+	@RequestMapping(value = "/greeting", method = { RequestMethod.GET, RequestMethod.POST })
+	public ModelAndView greeting(String name) {
+
+		if (name == null || name == "") {
+			name = "world";
+		}
+		ModelAndView mov = new ModelAndView();
+		mov.addObject("greeting", "Hello " + name + "!");
+		mov.setViewName("/greeting");
+		return mov;
+	}
+
+}
