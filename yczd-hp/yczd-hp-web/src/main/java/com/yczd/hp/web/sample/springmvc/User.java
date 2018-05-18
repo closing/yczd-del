33dc19a8e300fe5d0a8e6aa796cb3f7e15af6cfa
closing <zhu.yuxing@hotmail.com>
2018-05-18 17:41:52 +0900

    Create User.java

---
+++ yczd-hp/yczd-hp-web/src/main/java/com/yczd/hp/web/sample/springmvc/User.java
@@ -0,0 +1,42 @@
+package com.yczd.hp.web.sample.springmvc;
+
+public class User {
+	private Integer id;
+
+	private String userName;
+	private String email;
+	private String passowrd;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassowrd() {
+		return passowrd;
+	}
+
+	public void setPassowrd(String passowrd) {
+		this.passowrd = passowrd;
+	}
+
+}
