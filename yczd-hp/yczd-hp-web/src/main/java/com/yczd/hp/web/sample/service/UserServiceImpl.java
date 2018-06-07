7e4d28bdcf2403519c6ebb9c6302dca5fb211d9b
closing <zhu.yuxing@hotmail.com>
2018-06-07 18:39:02 +0900

    Create UserServiceImpl.java

---
+++ yczd-hp/yczd-hp-web/src/main/java/com/yczd/hp/web/sample/service/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package com.yczd.hp.web.sample.service;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.yczd.hp.web.sample.dao.IUserDao;
+import com.yczd.hp.web.sample.daodata.User;
+
+@Service("userService")
+public class UserServiceImpl implements IUserService {
+
+	@Resource
+	private IUserDao userDao;
+
+	@Override
+	public User getUserById(int id) {
+		return this.userDao.selectByPrimaryKey(id);
+
+	}
+
+}
